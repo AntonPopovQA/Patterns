2a275f3c5ce5b30ab591440a6570d35e499facfd
Антон Попов <aspirevx74@gmail.com>
2022-08-18 20:49:00 +0300

    TestFinal

--- src/test/java/ru/netology/test/PatternsTest.java
+++ src/test/java/ru/netology/test/PatternsTest.java
@@ -42,14 +42,18 @@ public class PatternsTest {
         $("[data-test-id=success-notification]")
                 .shouldHave(exactText("Успешно! Встреча успешно запланирована на " + firstMeetingDate));
         $x("//*[contains(text(),'Запланировать')]").click();
-        $("[data-test-id=replan-notification]")
-                .shouldHave(exactText("Необходимо подтверждение У вас уже запланирована встреча на другую дату. Перепланировать?"));
-        $("[data-test-id=date] .input__control").doubleClick().sendKeys(secondMeetingDate);
-        $("[data-test-id=replan-notification]")
-                .shouldHave(text("Перепланировать"), Duration.ofSeconds(10))
+        $("[data-test-id = replan-notification]")
+                .shouldHave(text("Необходимо подтверждение"), Duration.ofSeconds(15))
                 .shouldBe(visible);
-        $("[data-test-id=replan-notification] .button").click();
-        $("[data-test-id=success-notification]")
-               .shouldHave(exactText("Успешно! Встреча успешно запланирована на " + secondMeetingDate));
+        $("[data-test-id = replan-notification] .notification__content")
+                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"), Duration.ofSeconds(15))
+                .shouldBe(visible);
+        $("[data-test-id = replan-notification] .button")
+                .shouldHave(text("Перепланировать"), Duration.ofSeconds(15))
+                .shouldBe(visible);
+        $("[data-test-id = replan-notification] .button").click();
+        $("[data-test-id = success-notification]")
+                .shouldHave(text("Успешно!"), Duration.ofSeconds(15))
+                .shouldBe(visible);
     }
 }
